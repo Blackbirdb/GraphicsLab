ac38d6ee326ae1986389cad9b20d72f3c07ecd26
BaodaiHuang <bdhuang22@m.fudan.edu.cn>
2025-04-25 15:06:42 +0800

    finish lab2, I guess?

--- starter2/src/Renderer.cpp
+++ starter2/src/Renderer.cpp
@@ -15,6 +15,13 @@ Renderer::Renderer(const ArgParser &args) : _args(args),
 {
 }
 
+/**
+ * Expected behavior for super-sampling:
+ * 1. -jitter=true, -filter=false: Jittered super-sampling, no filtering, return image with resolution (3 * w, 3 * h)
+ * 2. -jitter=false, -filter=true: Regular super-sampling with Gaussian filter
+ * 3. -jitter=true, -filter=true: Jittered super-sampling with Gaussian filter
+ */
+
 void Renderer::Render()
 {
     int w = _args.width;
@@ -24,36 +31,45 @@ void Renderer::Render()
     Image nimage(w, h);
     Image dimage(w, h);
 
-    // loop through all the pixels in the image
-    // generate all the samples
+    if (!_args.jitter && !_args.filter){
+        // no super-sampling
+        image = Image(w, h);
+        nimage = Image(w, h);
+        dimage = Image(w, h);
 
-    // This look generates camera rays and callse traceRay.
-    // It also write to the color, normal, and depth images.
-    // You should understand what this code does.
-    Camera *cam = _scene.getCamera();
-    for (int y = 0; y < h; ++y)
-    {
-        float ndcy = 2 * (y / (h - 1.0f)) - 1.0f;
-        for (int x = 0; x < w; ++x)
-        {
-            float ndcx = 2 * (x / (w - 1.0f)) - 1.0f;
-            // Use PerspectiveCamera to generate a ray.
-            // You should understand what generateRay() does.
-            Ray r = cam->generateRay(Vector2f(ndcx, ndcy));
+        vanillaRendering(w, h, image, nimage, dimage);
+    }
 
-            Hit h;
-            Vector3f color = traceRay(r, cam->getTMin(), _args.bounces, h);
+    else{
+        // super-sampling
+        int super_w = w * 3;
+        int super_h = h * 3;
 
-            image.setPixel(x, y, color);
-            nimage.setPixel(x, y, (h.getNormal() + 1.0f) / 2.0f);
-            float range = (_args.depth_max - _args.depth_min);
-            if (range)
-            {
-                dimage.setPixel(x, y, Vector3f((h.t - _args.depth_min) / range));
-            }
+        Image superImage(super_w, super_h);
+        Image superNImage(super_w, super_h);
+        Image superDImage(super_w, super_h);
+
+        // jittering
+        if (_args.jitter){
+            jitteredRendering(super_w, super_h,
+                superImage, superNImage, superDImage);
+        }
+        else{
+            vanillaRendering(super_w, super_h,
+                superImage, superNImage, superDImage);
+        }
+        // filtering
+        if (_args.filter){
+            image = ApplyGaussianFilter(superImage, w, h);
+            nimage = ApplyGaussianFilter(superNImage, w, h);
+            dimage = ApplyGaussianFilter(superDImage, w, h);
+        }
+        else{
+            image = superImage;
+            nimage = superNImage;
+            dimage = superDImage;
         }
     }
-    // END SOLN
 
     // save the files
     if (_args.output_file.size())
@@ -128,6 +144,107 @@ Renderer::traceRay(const Ray &r,
     }
     else
     {
+        h.t = 0;
         return _scene.getBackgroundColor(r.getDirection());
+    }
+}
+
+/**
+ * Applies Gaussian filter to a super sampled image.
+ * Returns downsampled image with resolution (w, h).
+ */
+Image Renderer::ApplyGaussianFilter(const Image& img, int w, int h){
+    Image result(w, h);
+    float kernel[3][3] = { {1,2,1}, {2,4,2}, {1,2,1} };
+    float kernelSum = 16.0f;
+
+    for (int y = 0; y < h; ++y){
+        for (int x = 0; x < w; ++x){
+            Vector3f blurred = Vector3f::ZERO;
+
+            for (int dy = -1; dy <= 1; ++dy){
+                for (int dx = -1; dx <= 1; ++dx){
+                    int sx = clamp(x * 3 + dx, 0, img.getWidth() - 1);
+                    int sy = clamp(y * 3 + dy, 0, img.getHeight() - 1);
+
+                    blurred += img.getPixel(sx, sy) * kernel[dy + 1][dx + 1];
+                }
+            }
+
+            result.setPixel(x, y, blurred / kernelSum);
+        }
+    }
+
+    return result;
+}
+
+float Renderer::clamp (float x, float min, float max){
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
+
+/**
+ * Vanilla rendering. Renders image, nimage and dimage by modifying the
+ * passed references to images.
+ */
+void Renderer::vanillaRendering(int w, int h,
+                                Image& image, Image& nimage, Image& dimage){
+
+    Camera* cam = _scene.getCamera();
+
+    for (int y = 0; y < h; ++y){
+
+        float ndcy = 2 * (y / (h - 1.0f)) - 1.0f;
+
+        for (int x = 0; x < w; ++x){
+            
+            float ndcx = 2 * (x / (w - 1.0f)) - 1.0f;
+            Ray r = cam->generateRay(Vector2f(ndcx, ndcy));
+
+            Hit h;
+            Vector3f color = traceRay(r, cam->getTMin(), _args.bounces, h);
+
+            image.setPixel(x, y, color);
+            nimage.setPixel(x, y, (h.getNormal() + 1.0f) / 2.0f);
+            float range = (_args.depth_max - _args.depth_min);
+            if (range){
+                dimage.setPixel(x, y, Vector3f((h.t - _args.depth_min) / range));
+            }
+        }
+    }
+}
+
+/**
+ * Jittered rendering. Assumes w, h are the super-sampled size of the images.
+ */
+void Renderer::jitteredRendering(int w, int h,
+                                 Image& image, Image& nimage, Image& dimage){
+    
+    Camera* cam = _scene.getCamera();
+
+    for (int y = 0; y < w; ++y){
+        for (int x = 0; x < h; ++x){
+            
+            float jitter_x = (x + drand48()) / (w - 1.0f); 
+            float jitter_y = (y + drand48()) / (h - 1.0f);
+
+            float ndcx = 2.0f * jitter_x - 1.0f;
+            float ndcy = 2.0f * jitter_y - 1.0f;
+
+            Ray r = cam->generateRay(Vector2f(ndcx, ndcy));
+
+            Hit hit;
+            Vector3f color = traceRay(r, cam->getTMin(), _args.bounces, hit);
+
+            image.setPixel(x, y, color);
+            nimage.setPixel(x, y, (hit.getNormal() + 1.0f) / 2.0f);
+
+            float range = _args.depth_max - _args.depth_min;
+            if (range > 0){
+                dimage.setPixel(x, y, Vector3f((hit.t - _args.depth_min) / range));
+            }
+        }
     }
 }
--- starter2/src/Renderer.h
+++ starter2/src/Renderer.h
@@ -10,18 +10,27 @@ class Hit;
 class Vector3f;
 class Ray;
 
-class Renderer
-{
-  public:
-    // Instantiates a renderer for the given scene.
-    Renderer(const ArgParser &args);
-    void Render();
-  private:
-    Vector3f traceRay(const Ray &ray, float tmin, int bounces, 
-                      Hit &hit) const;
+class Renderer{
+public:
+	// Instantiates a renderer for the given scene.
+	Renderer(const ArgParser& args);
+	void Render();
+private:
+	Vector3f traceRay(const Ray& ray, float tmin, int bounces,
+		Hit& hit) const;
 
-    ArgParser _args;
-    SceneParser _scene;
+	Image ApplyGaussianFilter(const Image& img, int w, int h);
+
+	float clamp(float x, float min, float max);
+
+	void vanillaRendering(int w, int h,
+		Image& image, Image& nimage, Image& dimage);
+	
+	void jitteredRendering(int w, int h,
+		Image& image, Image& nimage, Image& dimage);
+	
+	ArgParser _args;
+	SceneParser _scene;
 };
 
 #endif // RENDERER_H
